b62a1d2585a8798bb11aa0668ab44a67333b3638
Youssef Jemmane <65173393+YoussefJemmane@users.noreply.github.com>
2023-10-02 11:58:09 +0100

    updqte 2

--- Algorithmic/algo.js
+++ Algorithmic/algo.js
@@ -1,5 +1,17 @@
 function algo() {
     for(i=1;i<=100;i++){
+		if(i%3 === 0 && i%5 === 0 && i%7 === 0){
+			console.log("Hello World Yoo");
+		}
+		if(i%5 === 0 && i%7 === 0){
+			console.log("World Yoo");
+		}
+		if(i%3 === 0 && i%7 === 0){
+			console.log("Hello Yoo");
+		}
+		if(i%3 === 0 && i%5 === 0 ){
+			console.log("Hello World");
+		}
 		if(i%3 === 0){
 			console.log("Hello");
 		}else if(i%5 === 0){
--- Algorithmic/algo.php
+++ Algorithmic/algo.php
@@ -1,13 +1,23 @@
-<?php 
-  for($i=1;$i<=100;$i++){
-    if($i%3 === 0){
-      echo "Hello \x20";
-    }else if($i%5 === 0){
-      echo "World \x20";
-    }else if($i%7 === 0){
-      echo "Too \x20 ";
-    } else{
-      echo $i. "\x20";
+<?php
+
+  for ($i=1;$i<=100;$i++){
+    if ($i%3 === 0 && $i%5 === 0 && $i%7 === 0) {
+        echo "Hello World Yoo\n";
+    } elseif ($i%3 === 0 && $i%5 === 0) {
+        echo "Hello World\n";
+    } elseif ($i%3 === 0 && $i%7 === 0) {
+        echo "Hello Yoo\n";
+    } elseif ($i%5 === 0 && $i%7 === 0) {
+        echo "World Yoo\n";
+    } elseif ($i%3 === 0) {
+        echo "Hello\n";
+    } elseif ($i%5 === 0) {
+        echo "World\n";
+    } elseif ($i%7 === 0) {
+        echo "Yoo\n";
+    } else {
+        echo $i . "\n";
     }
   }
-?> 
+
+?>
